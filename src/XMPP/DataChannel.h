9d23ef685884fb3c0ce8874f8bc23951d7a24969
Remko Tronçon <git@el-tramo.be>
2009-06-28 23:24:49 +0200

    Small clarifications.

--- src/XMPP/DataChannel.h
+++ src/XMPP/DataChannel.h
@@ -8,5 +8,5 @@ using namespace std;
 class DataChannel {
 	public:
 		virtual void sendData(const string& data) = 0; 
-		boost::signal<void (const string&)> onDataReceived; /* (*) The signal <literal>onDataReceived</literal> has one <literal>string</literal> parameter (and no return value). When the signal is emitted, the <literal>string</literal> argument will be passed to the connected slot method. */
+		boost::signal<void (const string&)> onDataReceived; /* (*) The signal <literal>onDataReceived</literal> has one <literal>string</literal> parameter (and no return value). When the signal is emitted, the <literal>string</literal> argument containing the data received will be passed to the connected slot method. */
 };
